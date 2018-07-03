312050c6461427b6d73dcaede1ee0c66713f87d3
Jessica Rosenfield <jrosenfield@grailbio.com>
2018-07-03 00:24:36 +0000

    lims/ui: remove date types from omni for now
    Summary:
    Because the date picker is not working
    after the current omni refactor. Date types won't
    be included in the limited rollout of the omni bar.
    Added FIXME's for change to be included in next release.
    
    Reviewers: jsingh
    
    Reviewed By: jsingh
    
    Subscribers: aahn
    
    Differential Revision: https://phabricator.grailbio.com/D15763
    
    fbshipit-source-id: 728d064

--- src/utils/api-utils/api-constants.js
+++ src/utils/api-utils/api-constants.js
@@ -3,6 +3,7 @@
 export const percentChar = "%25"; // %25 is `%`. This provides the LIKE query option.
 export const doubleAmpersand = "%26%26"; // %26%26 is `&&`.
 export const doublePipe = "||";
+
 export const LIKE_TEXT_SEARCH_TYPE = Symbol("like-text");
 export const NUMERIC_SEARCH_TYPE = Symbol("numeric");
 export const BOOLEAN_SEARCH_TYPE = Symbol("boolean");
@@ -12,3 +13,5 @@ export const DATE_SEARCH_TYPE = Symbol("date");
 export const DATETIME_SEARCH_TYPE = Symbol("datetime");
 export const FULL_ID_SEARCH_TYPE = Symbol("full-id");
 export const LIKE_ID_SEARCH_TYPE = Symbol("like-id");
+
+export const DATE_SEARCH_TYPES = [DATE_SEARCH_TYPE, DATETIME_SEARCH_TYPE];
