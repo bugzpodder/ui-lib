3090dd02dd86c7ea7ea01e6c1dcea8ea69951226
Vlad <vsterzhanov@grailbio.com>
2018-07-05 18:56:08 +0000

    pipeline/ui adapt into a lims deployment architecture
    Summary:
    Add a bazelized docker build config.
    Point to it from lims terraform.
    Adjust the links inside webapp itself to reflect the new url scheme.
    
    Ref T8272
    
    Test Plan: create a new lims deployment from terraform config, verify that [deployment].lims.grail.com/pipeline-ui works
    
    Reviewers: treaster, aahn, jsingh, nsawas
    
    Reviewed By: treaster, jsingh, nsawas
    
    Subscribers: jsingh, cbain
    
    Maniphest Tasks: T8272
    
    Differential Revision: https://phabricator.grailbio.com/D15673
    
    fbshipit-source-id: 977d566

--- src/constants/sidebar-content.js
+++ src/constants/sidebar-content.js
@@ -116,8 +116,8 @@ export const sidebarItems: (SidebarItem | ParentSidebarItem)[] = [
 			},
 			{
 				name: "Previous Runs",
-				domain: PIPELINE,
-				path: "",
+				domain: LIMS,
+				path: "/pipeline-ui",
 			},
 		],
 	},
