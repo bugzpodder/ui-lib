45420624b6ecb9360d7daeffa94fc4b40a9f67e2
Alice Yang <ayang@grailbio.com>
2018-11-19 07:41:32 +0000

    ui/lims: rename "Study Sample" to "Non-Patient Sample"
    Summary: Ref T13361
    
    Test Plan: Ran nav cypress test.
    
    Reviewers: jsingh, O12 LIMS UI, O13 UI Common, jzhao
    
    Reviewed By: jsingh, O12 LIMS UI, O13 UI Common, jzhao
    
    Maniphest Tasks: T13361
    
    Differential Revision: https://phabricator.grailbio.com/D21599
    
    fbshipit-source-id: ef948a9

--- src/constants/sidebar-content.js
+++ src/constants/sidebar-content.js
@@ -54,12 +54,12 @@ export const sidebarItems: (SidebarItemLink | SidebarItemParent | SidebarItemPla
         path: "/lims/print-labels",
       },
       {
-        name: "Study Samples",
+        name: "Non-Patient Samples",
         domain: LIMS,
         path: "/lims/study-samples",
       },
       {
-        name: "Create Study Samples",
+        name: "Create Non-Patient Samples",
         domain: LIMS,
         path: "/lims/create-study-samples",
       },
