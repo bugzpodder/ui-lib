43597ba28adb3b29c611a55b0952f491b76fc2fc
Michael Hang <mhang@grailbio.com>
2019-01-23 02:00:32 +0000

    edc/web-client: update vitamin d trf validation rules
    Summary: Add TRF validation for the Vitamin D TRF form. Fixes T15236.
    
    Test Plan: integration tests
    
    Reviewers: mward, sbhattacharjee, jsingh, jzhao, O13 UI Common
    
    Reviewed By: mward, jsingh, O13 UI Common
    
    Maniphest Tasks: T15236
    
    Differential Revision: https://phabricator.grailbio.com/D24204
    
    fbshipit-source-id: c17112d

--- src/constants/date-constants.js
+++ src/constants/date-constants.js
@@ -4,6 +4,7 @@ export const EPOCH_DATE = "1970-01-01";
 export const EPOCH_DATE_TIME = "1970-01-01T08:00:00.000Z";
 export const DATE_FORMAT = "YYYY-MM-DD";
 export const DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
+export const ISO8601_DATE_TIME_FORMAT = "YYYY-MM-DDTHH:mm:ssZ";
 
 /**
  * ISO-8601 input masks.
