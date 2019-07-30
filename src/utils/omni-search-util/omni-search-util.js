bugzpodder/ui-lib

commit 291cfb94febdd12030dbfa80754186d05a4a9ec5

lims/ui: don't display extra blank chip
Summary:
When a user enters a search term with an extra comma, the UI should only
display omni chips that match the actual search, and no extraneous blank
chips.
{F609983}
Fix T13209
Also, standardize on space in omni search, which is more natural during editing.

Test Plan: Unit, test in UI.

Reviewers: margi, jrosenfield, pmoradi, nsawas, O13 UI Common

Reviewed By: nsawas, O13 UI Common

Subscribers: aahn

Maniphest Tasks: T13209

Differential Revision: https://phabricator.grailbio.com/D21166

fbshipit-source-id: 1be1484

src/utils/omni-search-util/omni-search-util.js
@@ -8,6 +8,7 @@ const searchKey = /(([^:]*\s*)\s)?([\w-.]+):/gm;
 
 const addItemToArrayMap = (key: string, value: string, arrayMap: Map<string, Array<string>>) => {
   if (value === "" || isValueValid(value)) {
+    value = value.trim();
     const previousValues = arrayMap.get(key);
     if (previousValues === undefined) {
       arrayMap.set(key, [value]);
@@ -76,8 +77,7 @@ export const getSearchValuesFromOmniText = (searchDefs: SearchDefs, omniText: st
       values = values.map(value => value.trim());
     }
     if (values.length) {
-      // TODO: consider including a space (", ") when joining.
-      const searchValue = values.join(",");
+      const searchValue = values.join(", ");
       searchValues.set(index, searchValue);
     }
   });
@@ -101,7 +101,7 @@ export const getOmniTextFromSearchValues = (searchDefs: SearchDefs, searchValues
       if (index !== 0) {
         const key = getKeysForSearchDef(searchDef)[0];
         // $FlowFixMe: isValueValid call means omniValue is defined
-        omniValue = `${key}:${omniValue}`;
+        omniValue = `${key}: ${omniValue}`;
       }
       omniValues.push(omniValue);
     }
@@ -109,14 +109,17 @@ export const getOmniTextFromSearchValues = (searchDefs: SearchDefs, searchValues
   return omniValues.join(" ");
 };
 
-export const getOmniTextFromKeyValues = (keyValues: Array<KeyValue>) => keyValues.map(({ key, value }) => `${key}:${value}`).join(" ");
+export const getOmniTextFromKeyValues = (keyValues: Array<KeyValue>) => keyValues.map(({ key, value }) => `${key}: ${value}`).join(" ");
 
 export const getItemsFromOmniValue = (omniValue: string = ""): Array<string> => {
   const parsedItems: Array<string> = [];
   if (!isValueValid(omniValue)) {
     return parsedItems;
   }
-  return omniValue.split(",").map(value => value.trim());
+  return omniValue
+    .split(",")
+    .map(value => value.trim())
+    .filter(value => value !== "");
 };
 
 export const getSearchOptions = (searchDefs: SearchDefs, searchValues: SearchValues): SearchOptionsV2 => {

src/utils/omni-search-util/omni-search-util.spec.js
@@ -49,20 +49,20 @@ describe("getOmniTextFromSearchValues", () => {
     const searchValues = new Map();
     searchValues.set(0, "1");
     searchValues.set(2, "34, 12");
-    const expectedOmniText = "1 lot:34, 12";
+    const expectedOmniText = "1 lot: 34, 12";
     expect(getOmniTextFromSearchValues(searchDefs, searchValues)).toEqual(expectedOmniText);
   });
   it("skips missing values", () => {
     const searchValues = new Map();
     searchValues.set(2, "34,");
-    const expectedOmniText = "lot:34,";
+    const expectedOmniText = "lot: 34,";
     expect(getOmniTextFromSearchValues(searchDefs, searchValues)).toEqual(expectedOmniText);
   });
   it("skips empty strings", () => {
     const searchValues = new Map();
     searchValues.set(0, "");
     searchValues.set(2, "34,");
-    const expectedOmniText = "lot:34,";
+    const expectedOmniText = "lot: 34,";
     expect(getOmniTextFromSearchValues(searchDefs, searchValues)).toEqual(expectedOmniText);
   });
 });
@@ -79,7 +79,7 @@ describe("getOmniTextFromKeyValues", () => {
         value: "34, 12",
       },
     ];
-    const expectedOmniText = "part:1 lot:34, 12";
+    const expectedOmniText = "part: 1 lot: 34, 12";
     expect(getOmniTextFromKeyValues(keyValues)).toEqual(expectedOmniText);
   });
 });
@@ -95,6 +95,11 @@ describe("getSearchValuesFromOmniText", () => {
     const expectedSearchValues = new Map();
     expectedSearchValues.set(3, "ABC");
     expect(getSearchValuesFromOmniText(searchDefs, "someEnum:  ABC")).toEqual(expectedSearchValues);
+  });
+  it("should extract omni field", () => {
+    const expectedSearchValues = new Map();
+    expectedSearchValues.set(0, "ABC");
+    expect(getSearchValuesFromOmniText(searchDefs, " ABC")).toEqual(expectedSearchValues);
   });
   // it("accepts multiple values under the same key", () => {
   //   const expectedSearchValues = new Map();
@@ -113,6 +118,12 @@ describe("getSearchValuesFromOmniText", () => {
     expect(getSearchValuesFromOmniText(searchDefs, "")).toEqual(expectedSearchValues);
     expect(getSearchValuesFromOmniText(null, "foobar")).toEqual(expectedSearchValues);
   });
+  it("handles non omni searchDefs", () => {
+    const [__omniDef, ...remainingDefs] = searchDefs;
+    const expectedSearchValues = new Map();
+    expectedSearchValues.set(1, "34");
+    expect(getSearchValuesFromOmniText(remainingDefs, "lot: 34")).toEqual(expectedSearchValues);
+  });
 });
 
 describe("getSearchOptions", () => {
@@ -130,7 +141,7 @@ describe("getSearchOptions", () => {
       aliases: ["lot"],
       description: "Lot Num/Vendor Lot Num",
       searchFields: ["lotNumber", "vendorLotNumber"],
-      values: ["2", "", "3", "4"],
+      values: ["2", "3", "4"],
     },
   ];
   const searchValues = new Map();
@@ -146,11 +157,14 @@ describe("getSearchOptions", () => {
 });
 
 describe("getItemsFromOmniValue", () => {
-  it("can handle null/undfined args", () => {
+  it("can handle null/undefined args", () => {
     expect(getItemsFromOmniValue()).toEqual([]);
   });
   it("splits on comma and trims spaces", () => {
     expect(getItemsFromOmniValue(" 12 1,  34")).toEqual(["12 1", "34"]);
+  });
+  it("eliminates empty comma sections", () => {
+    expect(getItemsFromOmniValue(" 12 1, , 34")).toEqual(["12 1", "34"]);
   });
   // TODO(jrosenfield): add more testing coverage
 });
@@ -166,6 +180,6 @@ describe("getValueItemsFromSearchValues", () => {
     expect(getValueItemsFromSearchValues(searchDefs, searchValues, "anEnum")).toEqual([]);
   });
   it("gets search values for matching key", () => {
-    expect(getValueItemsFromSearchValues(searchDefs, searchValues, "lot")).toEqual(["2", "", "3", "4"]);
+    expect(getValueItemsFromSearchValues(searchDefs, searchValues, "lot")).toEqual(["2", "3", "4"]);
   });
 });

src/utils/url-utils/url-search.js
@@ -54,7 +54,7 @@ export const extractSearchValues = (searchOptions: SearchOptions): SearchOptionV
   return searchValues;
 };
 
-export const mergeSearchOptions = (searchOptions: SearchOptions, searchValues?: SearchOptionValues) => {
+export const mergeSearchOptions = (searchOptions: SearchOptions, searchValues?: SearchOptionValues): SearchOptions => {
   if (!searchValues) {
     return searchOptions;
   }

src/utils/url-utils/url-search.spec.js
@@ -138,6 +138,6 @@ describe("getUrlQueryForOptions", () => {
 describe("getOmniUrlQueryString", () => {
   it("should get url query", () => {
     const test = getOmniUrlQueryString([{ key: "test", value: "name" }]);
-    expect(test).toBe("omni=test%3Aname");
+    expect(test).toBe("omni=test%3A%20name");
   });
 });

commit 8233c923bacc4aa28f0e11ce4feb647fc2d72047

lims/ui: fix a bug introduced in D21166
Summary:
D21166 broke the ability to type spaces in omni fields.
Fix the utilities that handle this.
Ref T13209
Also, update an unrelated snapshot.

Test Plan: Run Cypress, unit.

Reviewers: O13 UI Common, nsawas, jrosenfield, pmoradi, margi

Reviewed By: O13 UI Common, nsawas

Maniphest Tasks: T13209

Differential Revision: https://phabricator.grailbio.com/D21178

fbshipit-source-id: a1d098a

src/utils/omni-search-util/omni-search-util.js
@@ -8,7 +8,8 @@ const searchKey = /(([^:]*\s*)\s)?([\w-.]+):/gm;
 
 const addItemToArrayMap = (key: string, value: string, arrayMap: Map<string, Array<string>>) => {
   if (value === "" || isValueValid(value)) {
-    value = value.trim();
+    const removedLeadingWhitespaceMatch = /\s*(.*)/.exec(value);
+    value = removedLeadingWhitespaceMatch ? removedLeadingWhitespaceMatch[1] : value;
     const previousValues = arrayMap.get(key);
     if (previousValues === undefined) {
       arrayMap.set(key, [value]);