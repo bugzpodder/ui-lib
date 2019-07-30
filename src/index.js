bugzpodder/ui-lib

commit 12ce962f3bc47e12c9af4d6069381e0505ac1f7c

lims/ui: only show prefix in sample details page
Summary: {F574598}

Test Plan: UI.

Reviewers: margi, jsingh, O12 LIMS UI, O13 UI Common

Reviewed By: margi, O12 LIMS UI, O13 UI Common

Subscribers: rhuang

Maniphest Tasks: T12550

Differential Revision: https://phabricator.grailbio.com/D20653

fbshipit-source-id: d13b94f

src/index.js
@@ -99,7 +99,9 @@ export {
   // $FlowFixMe: exports are missing in ModuleNamespace for export {} from
   formatPercent,
 } from "./utils/string-utils";
-export { sanitizeId, getInputBarcode } from "./utils/id-utils";
+export {
+  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix,
+} from "./utils/id-utils";
 export {
   expandSearchValues,
   extractSearchValues,

src/utils/id-utils/id-util.js
@@ -14,17 +14,22 @@ export const sanitizeId = (id: string = "") => {
   return id.replace(/-/g, "");
 };
 
+export const getSamplePrefix = (id: string) => id.substr(0, id.length - 1);
+
+const getSampleSuffix = (id: string) => id[id.length - 1];
+
+export const getSampleLabel = (id: string) => `${getSamplePrefix(id)}-${getSampleSuffix(id)}`;
+
 export const getInputBarcode = (id: string = "", previousId: string = "") => {
   id = id.trim();
   if (!previousId) {
     return id;
   }
 
-  const prefix = id.substr(0, id.length - 1);
-  const suffix = id[id.length - 1];
-  const previousLabelPrefix = previousId.substr(0, previousId.length - 1);
+  const prefix = getSamplePrefix(id);
+  const previousLabelPrefix = getSamplePrefix(previousId);
   if (prefix === previousLabelPrefix) {
-    return `${prefix}-${suffix}`;
+    return getSampleLabel(id);
   }
   return id;
 };

src/utils/id-utils/index.js
@@ -1,4 +1,4 @@
 // @flow
-import { getInputBarcode, sanitizeId } from "./id-util";
-
-export { sanitizeId, getInputBarcode };
+export {
+  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix,
+} from "./id-util";

src/utils/id-utils/id-util.spec.js
@@ -1,6 +1,8 @@
 // @flow
 
-import { getInputBarcode, sanitizeId } from "./id-util";
+import {
+  getInputBarcode, getSampleLabel, getSamplePrefix, sanitizeId,
+} from "./id-util";
 
 describe("sanitizeId", () => {
   it("should sanitize all normal sample ids", () => {
@@ -30,5 +32,17 @@ describe("getInputBarcode", () => {
   });
   it("should return the given id when the previous id doesn't match", () => {
     expect(getInputBarcode("S000100", "P000100")).toBe("S000100");
+  });
+});
+
+describe("getSampleLabel", () => {
+  it("returns hyphenated sample label", () => {
+    expect(getSampleLabel("P000101")).toBe("P00010-1");
+  });
+});
+
+describe("getSamplePrefix", () => {
+  it("returns sample prefix", () => {
+    expect(getSamplePrefix("P000101")).toBe("P00010");
   });
 });

commit ba4143c388ad3a0fc2cbde7491ed85982f109476

lims/ui: correct a typo, prefix -> suffix
Summary:
Fix a typo introduced in D20653
Ref T9966

Test Plan: Cypress

Reviewers: jzhao, rhuang, margi, O12 LIMS UI, O13 UI Common

Reviewed By: margi, O12 LIMS UI, O13 UI Common

Maniphest Tasks: T9966

Differential Revision: https://phabricator.grailbio.com/D20684

fbshipit-source-id: 1a80e7c

src/index.js
@@ -100,7 +100,7 @@ export {
   formatPercent,
 } from "./utils/string-utils";
 export {
-  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix,
+  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix, getSampleSuffix,
 } from "./utils/id-utils";
 export {
   expandSearchValues,

src/utils/id-utils/id-util.js
@@ -16,7 +16,7 @@ export const sanitizeId = (id: string = "") => {
 
 export const getSamplePrefix = (id: string) => id.substr(0, id.length - 1);
 
-const getSampleSuffix = (id: string) => id[id.length - 1];
+export const getSampleSuffix = (id: string) => id[id.length - 1];
 
 export const getSampleLabel = (id: string) => `${getSamplePrefix(id)}-${getSampleSuffix(id)}`;
 

src/utils/id-utils/index.js
@@ -1,4 +1,4 @@
 // @flow
 export {
-  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix,
+  sanitizeId, getInputBarcode, getSampleLabel, getSamplePrefix, getSampleSuffix,
 } from "./id-util";