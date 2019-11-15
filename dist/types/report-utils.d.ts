61276181afffcc78d8bf550c3dc61e35c3a1c522
Jack Zhao <yzhao@grailbio.com>
2019-11-15 02:23:02 +0000

    lims/ui: use configs to handle starting/building lims client
    Summary:
    There were two ways of starting lims client.  Consolidate into one since the other approach doesn't really work anymore.
    
    Rebuild common-private types for build commands to succeed. Building common-private needs lib types
    
    Test Plan: yarn build:lims-client && yarn start:lims-client
    
    Reviewers: lkong, nsawas, ecarrel, O12 LIMS UI, O13 UI Common
    
    Reviewed By: lkong, nsawas, O12 LIMS UI, O13 UI Common
    
    Subscribers: cji
    
    Maniphest Tasks: T8000
    
    Differential Revision: https://phabricator.grailbio.com/D39245
    
    fbshipit-source-id: 56a54f7

--- dist/types/report-utils.d.ts
+++ dist/types/report-utils.d.ts
@@ -1,8 +1,8 @@
 export declare type ExportableColumn = {
-  exportHeaderName: string;
-  exportAccessor: string | ((x0: Record<string, any>) => string);
+    exportHeaderName: string;
+    exportAccessor: string | ((x0: Record<string, any>) => string);
 };
 export declare type ReportOptions = {
-  delimiter?: string;
-  fileMimeType?: string;
+    delimiter?: string;
+    fileMimeType?: string;
 };
